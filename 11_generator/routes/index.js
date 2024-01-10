454625bcfa7aa4108cd6382637f4f8537a59e9a9
ChenlingasMx <42308734@qq.com>
2024-01-10 19:41:25 +0800

    fix: 文件上传

--- 11_generator/routes/index.js
+++ 11_generator/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
+var formidable = require("formidable");
+var path = require("path");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/portrait", (req, res) => {
+  res.render("portrait");
+});
+
+router.post("/portrait", (req, res, next) => {
+  // 创建表单对象
+  const form = formidable({
+    multiples: true,
+    uploadDir: path.join(__dirname, "../public/images"),
+    keepExtensions: true,
+  });
+  // 解析请求报文
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    let url = "/images/" + files.portrait.newFilename;
+    res.send(url);
+  });
 });
 
 module.exports = router;
